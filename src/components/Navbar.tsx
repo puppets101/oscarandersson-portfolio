1479136b8dee83a6c3ddeee46801acaa57d05bce
puppets101 <oscand92@gmail.com>
2021-03-25 07:59:24 +0100

    restructured navbar + styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,55 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import SocialFollow from "./SocialFollow";
+import profileImg from "../assets/unnamed.png";
 
 function Navbar() {
+  const [active, setActive] = useState(false);
+
+  const onMenuClick = () => {
+    setActive(!active);
+  };
   return (
-    <header className='bg-green-800 bg-opacity-80 absolute z-10 w-full'>
-      <div className='container mx-auto flex items-center justify-between'>
-        <nav className='flex'>
-          <div>
-            <NavLink
-              to='/'
-              exact
-              activeClassName='text-white'
-              className='inline-flex items-center py-8 px-3 lg:mr-4 sm:mr-2 hover:text-gray-200 text-2xl font-bold font tracking-widest'
-            >
-              OscarAndersson
-            </NavLink>
-          </div>
-          <div>
-            <NavLink
-              to='/skills'
-              exact
-              activeClassName='text-white'
-              className='inline-flex items-center py-8 px-3 lg:mr-4 sm:mr-2 hover:text-gray-200 text-2xl font-bold font tracking-widest'
-            >
-              Skills
-            </NavLink>
-            <NavLink
-              to='/about'
-              exact
-              activeClassName='text-white'
-              className='inline-flex items-center py-8 px-3 lg:mr-4 sm:mr-2 hover:text-gray-200 text-2xl font-bold font tracking-widest'
-            >
-              About
-            </NavLink>
-            <NavLink
-              to='/contact'
-              exact
-              activeClassName='text-white'
-              className='inline-flex items-center py-8 px-3 lg:mr-4 sm:mr-2 hover:text-gray-200 text-2xl font-bold font tracking-widest'
-            >
-              Contact
-            </NavLink>
+    <header className='bg-green-800 bg-opacity-80 relative'>
+      <div className='max-w-7xl mx-auto flex items-center justify-between p-2.5'>
+        <div
+          onClick={onMenuClick}
+          className={`
+          text-gray-200 cursor-pointer
+        md:hidden uppercase
+        `}
+        >
+          Menu <i className='fas fa-angle-down'></i>
+        </div>
+        <nav
+          className={`
+          ${!active && "hidden"}
+          absolute flex flex-col top-full w-full left-0 z-20
+          md:static md:w-auto md:flex-row md:flex
+        `}
+        >
+          <div className='md:flex-row md:flex'>
+            <div className='md:mr-4'>
+              <NavLink
+                to='/'
+                exact
+                activeClassName='text-gray-200'
+                className='flex w-full text-xl font-bold uppercase hover:text-gray-200 cursor-pointer py-4 px-4'
+              >
+                Oscar Andersson
+              </NavLink>
+            </div>
+            <div className='md:mr-4'>
+              <NavLink
+                to='/skills'
+                exact
+                activeClassName='text-gray-200'
+                className='flex w-full text-xl font-bold uppercase hover:text-gray-200 cursor-pointer py-4 px-4'
+              >
+                Skills
+              </NavLink>
+            </div>
+            <div className='md:mr-4'>
+              <NavLink
+                to='/about'
+                exact
+                activeClassName='text-gray-200'
+                className='flex w-full text-xl font-bold uppercase hover:text-gray-200 cursor-pointer py-4 px-4'
+              >
+                About
+              </NavLink>
+            </div>
+            <div className='md:mr-4'>
+              <NavLink
+                to='/contact'
+                exact
+                activeClassName='text-gray-200'
+                className='flex w-full text-xl font-bold uppercase hover:text-gray-200 cursor-pointer py-4 px-4'
+              >
+                Contact
+              </NavLink>
+            </div>
           </div>
         </nav>
-        <div className='inline-flex py-3 px-3 my-6'>
+        <div className='py-5 px-5'>
           <SocialFollow />
         </div>
       </div>
     </header>
   );
 }
+
+// inline-flex items-center py-8 px-3 lg:mr-4 sm:mr-2 hover:text-gray-200 text-2xl font-bold font tracking-widest
 
 export default Navbar;
