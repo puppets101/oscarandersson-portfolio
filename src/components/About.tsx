d89f5b4bec141efb58f427b6dd071c384bcee2d0
puppets101 <oscand92@gmail.com>
2022-01-18 15:31:06 +0100

    test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import squareProfileImg from "../assets/squareprofileimg.png";
-import TypingTitle from "./TypingTitle";
+import React from 'react';
+import squareProfileImg from '../assets/squareprofileimg.png';
+import TypingTitle from './TypingTitle';
 
 interface Id {
   id: string;
@@ -8,25 +8,25 @@ interface Id {
 
 function About(props: Id) {
   return (
-    <div className="mt-24 max-w-5xl mx-auto">
-      <div id={props.id} className="bg-gray-100 px-1 sm:px-4">
-        <section className="container block md:flex mx-auto">
-          <div className="md:w-2/3">
-            <TypingTitle title="About me" classes="text-4xl font-bold" />
+    <div className='mt-24 max-w-5xl mx-auto'>
+      <div id={props.id} className='bg-gray-100 px-1 sm:px-4'>
+        <section className='container block md:flex mx-auto'>
+          <div className='md:w-2/3'>
+            <TypingTitle title='About me' classes='text-4xl font-bold' />
             <div>
               <div>
-                <p className="p-4 font-light leading-relaxed">
+                <p className='p-4 font-light leading-relaxed'>
                   Before I began my path to become a web developer, I've spent a
                   substantial part of my professional career in the sports
                   betting business, working for a company whose goal is to
-                  provides its customers the best bet of the day. Here I gained
+                  provide its customers the best bet of the day. Here I gained
                   much confidence working on a deadline. As the publisher of the
                   company's texts, I had a lot of responsibility, providing our
                   customers with the most valuable content each day. During the
                   past three years, I've also written my picks, gaining
                   knowledge in statistics, research and writing.
                 </p>
-                <p className="p-4 font-light leading-relaxed">
+                <p className='p-4 font-light leading-relaxed'>
                   Working in the sports betting industry has prepared me for the
                   fast-paced web-development environment, and I'm looking
                   forward to taking my experience into a new line of work.
@@ -34,11 +34,11 @@ function About(props: Id) {
               </div>
             </div>
           </div>
-          <div className="md:w-1/3 flex justify-center items-center my-4">
+          <div className='md:w-1/3 flex justify-center items-center my-4'>
             <img
               src={squareProfileImg}
-              alt="Me"
-              className="object-cover object-center h-64 w-64 rounded-full"
+              alt='Me'
+              className='object-cover object-center h-64 w-64 rounded-full'
             />
           </div>
         </section>
